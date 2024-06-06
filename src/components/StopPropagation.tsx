3b7b0a5383c2a2da1f7b8185003980cab9731b76
jhonatanCastor <jhonatanjhonatan907@gmail.com>
2024-06-06 15:48:24 -0300

    feature/use StopPropagation functions

---
+++ src/components/StopPropagation.tsx
@@ -0,0 +1,19 @@
+const StopPropagation = () => {
+
+  const handlerClick = (e: MouseEvent) => {
+    e.stopPropagation();
+    alert("Cliquei no botão")
+  }
+
+  return (
+    <>
+      <div onClick={() => alert('Cliquei na div') } style={{padding: 10, background: 'red'}}>
+        <button onClick={handlerClick}>
+          Texte button
+        </button>
+      </div>
+    </>
+  )
+}
+
+export default StopPropagation;
